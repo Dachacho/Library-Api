70c72f71458f5ea5e35e9d0b276b6e884ec8e866
Dachacho <arevadzedachi123@gmail.com>
2025-08-19 19:07:11 +0400

    made a book model

---
+++ Library Api/Data/LibraryContext.cs
@@ -0,0 +1,11 @@
+using Library_Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library_Api.Data;
+
+public class LibraryContext:DbContext
+{ 
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+}
