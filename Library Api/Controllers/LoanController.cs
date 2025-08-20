e5f44d6807e7050ed037f371244ec542c0e24b47
Dachacho <arevadzedachi123@gmail.com>
2025-08-20 18:38:31 +0400

    feat: added return endpoint for logic book copies managment in create and return

--- Library Api/Controllers/LoanController.cs
+++ Library Api/Controllers/LoanController.cs
@@ -34,9 +34,56 @@ public class LoanController : ControllerBase
     public async Task<ActionResult<Loan>> CreateLoan(Loan loan)
     {
         if(!ModelState.IsValid) return BadRequest(ModelState);
+        
+        var userExists = await _context.Users.AnyAsync(u => u.Id == loan.UserId);
+        if(!userExists) return BadRequest("User not found");
+        
+        var book = await _context.Books.FindAsync(loan.BookId);
+        if(book == null) return BadRequest("Book not found");
+
+        if (book.CopiesAvailable <= 0) return BadRequest("No copies avaliable");
+
+        if (loan.LoanDate > DateTime.Now)
+        {
+            return BadRequest("Loan date can not be in the future");
+        }
+
+        if (loan.ReturnDate != null && loan.ReturnDate < loan.LoanDate)
+        {
+            return BadRequest("Return date can not be in the future");
+        }
+        
+        var duplicateLoan = await _context.Loans.AnyAsync(l =>
+            l.UserId == loan.UserId &&
+            l.BookId == loan.BookId &&
+            l.ReturnDate == null);
+        
+        if (duplicateLoan)
+            return BadRequest("User already has an active loan for this book.");
+        
+        book.CopiesAvailable--;
+        
         _context.Loans.Add(loan);
         await _context.SaveChangesAsync();
         return  CreatedAtAction("GetLoan", new { id = loan.Id }, loan);
+    }
+
+    [HttpPost("{id}/return")]
+    public async Task<IActionResult> ReturnLoan(int id)
+    {
+        var loan = await _context.Loans.FindAsync(id);
+        if (loan == null) return NotFound();
+        if (loan.ReturnDate != null) return BadRequest("Loan already returned");
+        
+        loan.ReturnDate = DateTime.Now;
+        
+        var book = await _context.Books.FindAsync(loan.BookId);
+        if(book == null) return BadRequest("Book not found");
+        
+        book.CopiesAvailable++;
+        
+        await _context.SaveChangesAsync();
+        return NoContent();
     }
 
     [HttpPut("{id}")]
